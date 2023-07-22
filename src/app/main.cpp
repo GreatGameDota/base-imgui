62e47cd6db27aff58ada3fc09bdfaa5ac8de6261
GreatGameDota <17858650+GreatGameDota@users.noreply.github.com>
2023-07-22 16:30:50 -0500

    Add implot

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -10,7 +10,7 @@
 // Read comments in imgui_impl_vulkan.h.
 
 #include "imgui.h"
-#include "imgui_impl_sdl.h"
+#include "imgui_impl_sdl2.h"
 #include "imgui_impl_vulkan.h"
 #include <stdio.h>          // printf, fprintf
 #include <stdlib.h>         // abort
@@ -18,7 +18,51 @@
 #include <SDL_vulkan.h>
 #include <vulkan/vulkan.h>
 
+#include "implot.h"
+
 #include "Roboto-Regular.embed"
+
+// utility structure for realtime plot
+struct ScrollingBuffer {
+    int MaxSize;
+    int Offset;
+    ImVector<ImVec2> Data;
+    ScrollingBuffer(int max_size = 2000) {
+        MaxSize = max_size;
+        Offset = 0;
+        Data.reserve(MaxSize);
+    }
+    void AddPoint(float x, float y) {
+        if (Data.size() < MaxSize)
+            Data.push_back(ImVec2(x, y));
+        else {
+            Data[Offset] = ImVec2(x, y);
+            Offset = (Offset + 1) % MaxSize;
+        }
+    }
+    void Erase() {
+        if (Data.size() > 0) {
+            Data.shrink(0);
+            Offset = 0;
+        }
+    }
+};
+
+// utility structure for realtime plot
+struct RollingBuffer {
+    float Span;
+    ImVector<ImVec2> Data;
+    RollingBuffer() {
+        Span = 10.0f;
+        Data.reserve(2000);
+    }
+    void AddPoint(float x, float y) {
+        float xmod = fmodf(x, Span);
+        if (!Data.empty() && xmod < Data.back().x)
+            Data.shrink(0);
+        Data.push_back(ImVec2(xmod, y));
+    }
+};
 
 //#define IMGUI_UNLIMITED_FRAME_RATE
 #ifdef _DEBUG
@@ -380,6 +424,7 @@ int main(int, char**)
     // Setup Dear ImGui context
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
+    ImPlot::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
     //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
@@ -538,6 +583,49 @@ int main(int, char**)
         }
 
         {
+            ImGui::Begin("ImPlot Window");
+
+            ImGui::BulletText("Move your mouse to change the data!");
+            ImGui::BulletText("This example assumes 60 FPS. Higher FPS requires larger buffer size.");
+            static ScrollingBuffer sdata1, sdata2;
+            static RollingBuffer   rdata1, rdata2;
+            ImVec2 mouse = ImGui::GetMousePos();
+            static float t = 0;
+            t += ImGui::GetIO().DeltaTime;
+            sdata1.AddPoint(t, mouse.x * 0.0005f);
+            rdata1.AddPoint(t, mouse.x * 0.0005f);
+            sdata2.AddPoint(t, mouse.y * 0.0005f);
+            rdata2.AddPoint(t, mouse.y * 0.0005f);
+
+            static float history = 10.0f;
+            ImGui::SliderFloat("History", &history, 1, 30, "%.1f s");
+            rdata1.Span = history;
+            rdata2.Span = history;
+
+            static ImPlotAxisFlags flags = ImPlotAxisFlags_NoTickLabels;
+
+            if (ImPlot::BeginPlot("##Scrolling", ImVec2(-1, 150))) {
+                ImPlot::SetupAxes(NULL, NULL, flags, flags);
+                ImPlot::SetupAxisLimits(ImAxis_X1, t - history, t, ImGuiCond_Always);
+                ImPlot::SetupAxisLimits(ImAxis_Y1, 0, 1);
+                ImPlot::SetNextFillStyle(IMPLOT_AUTO_COL, 0.5f);
+                ImPlot::PlotShaded("Mouse X", &sdata1.Data[0].x, &sdata1.Data[0].y, sdata1.Data.size(), -INFINITY, 0, sdata1.Offset, 2 * sizeof(float));
+                ImPlot::PlotLine("Mouse Y", &sdata2.Data[0].x, &sdata2.Data[0].y, sdata2.Data.size(), 0, sdata2.Offset, 2 * sizeof(float));
+                ImPlot::EndPlot();
+            }
+            if (ImPlot::BeginPlot("##Rolling", ImVec2(-1, 150))) {
+                ImPlot::SetupAxes(NULL, NULL, flags, flags);
+                ImPlot::SetupAxisLimits(ImAxis_X1, 0, history, ImGuiCond_Always);
+                ImPlot::SetupAxisLimits(ImAxis_Y1, 0, 1);
+                ImPlot::PlotLine("Mouse X", &rdata1.Data[0].x, &rdata1.Data[0].y, rdata1.Data.size(), 0, 0, 2 * sizeof(float));
+                ImPlot::PlotLine("Mouse Y", &rdata2.Data[0].x, &rdata2.Data[0].y, rdata2.Data.size(), 0, 0, 2 * sizeof(float));
+                ImPlot::EndPlot();
+            }
+
+            ImGui::End();
+        }
+
+        {
             char buf[128];
             sprintf(buf, "Dear ImGui SDL2+Vulkan example (%.1f FPS)", ImGui::GetIO().Framerate);
             SDL_SetWindowTitle(window, buf);
@@ -582,6 +670,7 @@ int main(int, char**)
     ImGui_ImplVulkan_Shutdown();
     ImGui_ImplSDL2_Shutdown();
     ImGui::DestroyContext();
+    ImPlot::DestroyContext();
 
     CleanupVulkanWindow();
     CleanupVulkan();
